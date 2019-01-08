874038541b9a65ec6e66b698037b0e59ab9809f3
DESKTOP-8R0T6MJ\georg <george.razor1@gmail.com>
2019-01-08 00:23:44 -0600

    se añadió el contenido en el fragmento de perfil, también las transiciones en las cardview, se agregó una librería para mostrar el icono del perfil en forma circular.

--- app/src/main/java/com/george/platzigram/adapter/PictureAdapterRecyclerView.java
+++ app/src/main/java/com/george/platzigram/adapter/PictureAdapterRecyclerView.java
@@ -1,8 +1,13 @@
 package com.george.platzigram.adapter;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Explode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +16,7 @@ import android.widget.TextView;
 
 import com.george.platzigram.R;
 import com.george.platzigram.model.Picture;
+import com.george.platzigram.view.PictureDetailActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -41,6 +47,22 @@ public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdap
         pictureViewHolder.timeCard.setText(picture.getTime());
         pictureViewHolder.likeNumberCard.setText(picture.getLike_number());
         Picasso.get().load(picture.getPicture()).into(pictureViewHolder.pictureCard);
+
+        pictureViewHolder.pictureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, PictureDetailActivity.class);
+//ADD THE EFFECT WHEN YOU CLICK ON CARDVIEW TO SHOW THE DETAIL
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    Explode explode = new Explode();
+                    explode.setDuration(100);
+                    activity.getWindow().setExitTransition(explode);
+                    activity.startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(activity, v, activity.getString(R.string.transitionname_picture)).toBundle());
+                } else {
+                    activity.startActivity(intent);
+                }
+            }
+        });
 
     }
 
--- app/src/main/java/com/george/platzigram/view/PictureDetailActivity.java
+++ app/src/main/java/com/george/platzigram/view/PictureDetailActivity.java
@@ -1,7 +1,13 @@
 package com.george.platzigram.view;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.transition.Fade;
+import android.view.View;
 
 import com.george.platzigram.R;
 
@@ -11,5 +17,20 @@ public class PictureDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_detail);
+        showToolbar("", true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setEnterTransition(new Fade());
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    public void showToolbar(String tittle, boolean upButton) {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingToolbar);
+
     }
 }
